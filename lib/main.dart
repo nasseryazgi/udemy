000a067dbbb09a55cca612a29cdb0a3d06ef8e7e
Nasser Yazgi <85821198+nasseryazgi@users.noreply.github.com>
2022-12-06 19:18:59 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,9 +19,6 @@ class MyApp extends StatelessWidget
   @override
   Widget build(BuildContext context)
   {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
-    );
+    retur
   }
 }
